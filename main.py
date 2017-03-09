05bbdd31eed05d31238d274c37c7d1bf24d66c71
Lucas Romao <lucas.romao.silva@gmail.com>
2017-03-09 11:41:58 -0300

    started iterations, must add a way to stop when it converged

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def calculate_activation_func(y):
-    return 1.0 if y > 0.0 else 0.0
+    return 1 if y > 0.0 else 0
 
 #calculate the output given weight vector w and input vector x
 def calculate_output(w, x):
@@ -15,10 +15,12 @@ X is the matrix with all the inputs
 w is the weight vector
 '''
 def get_random_misclassfied_input(w, t, X):
-    misclassified_inputs = [X[:, i] for i in range(X.T) if calculate_output(w, X[:, i]) != t[i]]
+    misclassified_inputs = [[X[:, i], i] for i in range(len(X.T)) if calculate_output(w, X[:, i]) != t[i]]
+    print("misclassified inputs: ", misclassified_inputs)
     import random
-    random_index = random.uniform(0, len(misclassified_inputs) - 1)
-    return misclassified_inputs[random_index], calculate_output(w, misclassified_inputs[random_index]) 
+    random_index = random.randint(0, len(misclassified_inputs) - 1)
+    print("chosen index: ", random_index)
+    return misclassified_inputs[random_index], calculate_output(w, misclassified_inputs[random_index][0]) 
 
 
 '''
@@ -26,7 +28,19 @@ updates the weight vector given expected output t
 obtained output y, input x and learning rate eta
 '''
 def update_weights(w, t, y, x, eta):
-    w += eta*(t - y)*x
+    w = w + eta*(t - y)*x
     return w
 
+w = np.array([0, 0, 0])
+t = np.array([0, 0, 0, 1])
+X = np.array([[1, 0, 0], [1, 0, 1], \
+        [1, 1, 0], [1, 1, 1]]).T
+eta = 0.5
+
+while 1: 
+    misclassified_input, y = get_random_misclassfied_input(w, t, X)
+    index = misclassified_input[-1]
+    w = update_weights(w, t[index], y, misclassified_input[0], eta)
+    print("new weight vector", w)
+    
 
